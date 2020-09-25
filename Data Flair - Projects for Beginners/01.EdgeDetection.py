e454e1ebbd6c1dbff77db1ca9f051fd7be308768
Mickaël Brás Dias <66512649+mickael-bdias@users.noreply.github.com>
2020-09-25 15:17:12 +0100

    Commit: Projects for Beginners folder and EdgeDetection.py script

---
+++ Data Flair - Projects for Beginners/01.EdgeDetection.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+"""01.EdgeDetection.py:
+ The Python opencv library is mostly preferred for computer vision tasks.
+ You can detect all the edges of different objects of the image."""
+
+__author__ = "Mickaël Dias"
+__copyright__ = "Copyright 2020, OpenCV-projects-with-Python"
+__license__ = "MIT"
+__version__ = "0.0.1"
+__maintainer__ = "Mickaël Dias"
+__email__ = "industry4.0all.gmail.com"
+__status__ = "Production"
