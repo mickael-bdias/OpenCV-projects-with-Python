3c5dbb8496eaa156353d08fa4ec2c2fe9077a91e
Mickaël Brás Dias <66512649+mickael-bdias@users.noreply.github.com>
2020-09-26 15:50:38 +0100

    Commit: Resized image

--- Data Flair - Projects for Beginners/01.EdgeDetection.py
+++ Data Flair - Projects for Beginners/01.EdgeDetection.py
@@ -6,7 +6,6 @@
 
 import cv2
 
-
 __author__ = "Mickaël Dias"
 __copyright__ = "Copyright 2020, OpenCV-projects-with-Python"
 __license__ = "MIT"
@@ -15,12 +14,20 @@ __maintainer__ = "Mickaël Dias"
 __email__ = "industry4.0all.gmail.com"
 __status__ = "Development"
 
-path = "Resources/Images/lena.png"
-print(path)
-image = cv2.imread(path)
-print(image)
+path = "C:/Users/Mickael/PycharmProjects/OpenCV-projects-with-Python/Resources/Images/IMG_20200911_172937.jpg"
+imageOriginal = cv2.imread(path)
+print("Original Dimensions: ", imageOriginal.shape)
 
-cv2.imshow("lena", image)
+# Preserve Aspect Ratio - Downscale
+scale_percent = 10  # percent of original size
+height = int(imageOriginal.shape[0] * scale_percent / 100)
+width = int(imageOriginal.shape[1] * scale_percent / 100)
+dim = (width, height)
 
+# Resized image
+imageResized = cv2.resize(imageOriginal, dim, interpolation=cv2.INTER_AREA)
+print("Resized Dimensions: ", imageResized.shape)
+cv2.imshow("Resized", imageResized)
 
 cv2.waitKey(0)
+cv2.destroyAllWindows()
